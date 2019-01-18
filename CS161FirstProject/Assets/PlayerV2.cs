4ee16bb0f04c0c670c9b8461c08cf3bb2d331c78
Marco Cendejas <HomicideMojado@dhcp-v067-130.mobile.uci.edu>
2019-01-18 14:15:55 -0800

    added new inviciilty function to the player

--- CS161FirstProject/Assets/PlayerV2.cs
+++ CS161FirstProject/Assets/PlayerV2.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class PlayerV2 : MonoBehaviour
 {
     private Rigidbody2D player;
-    private bool can_jump = false;
+    private bool can_jump = false , gotHit = false;
     [SerializeField] private float speed = 2.0f;
     [SerializeField] private float airtime = 5.0f;
     [SerializeField] private int health = 3;
     [SerializeField] private int score = 0;
+    [SerializeField] private int invincibleDelay = 3;
 
     // Start is called before the first frame update
     void Start()
@@ -20,14 +21,17 @@ public class PlayerV2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float EPSILON = 0.000000000001f;
+        if (can_jump)
+            print("True");
+        else
+            print("False");
         float horizontal_translation = player.velocity.x;//Input.GetAxis("Horizontal") * speed;
-        if (System.Math.Abs(Input.GetAxis("Horizontal")) > EPSILON)
+        if (System.Math.Abs(Input.GetAxis("Horizontal")) > 0)
             horizontal_translation = Input.GetAxis("Horizontal") * speed;
-        print(player.velocity.x);
         float vertical_translation = player.velocity.y;//Input.GetAxis("Jump");
-        if (Input.GetKeyDown("up") && can_jump)
+        if (Input.GetAxis("Vertical") > 0 && can_jump)
         {
+            print("jump!!!");
             can_jump = false;
             player.AddForce(Vector2.up * airtime, ForceMode2D.Impulse);
         }
@@ -41,11 +45,16 @@ public class PlayerV2 : MonoBehaviour
             Destroy(collider.gameObject);
             ++score;
         }
-        if (collider.CompareTag("Enemy"))
+        if (!gotHit)
         {
-            print("Enemy");
-            Destroy(collider.gameObject);
-            --health;
+            if (collider.CompareTag("Enemy"))
+            {
+                gotHit = true;
+                print("Enemy");
+                Destroy(collider.gameObject);
+                --health;
+                Invoke("Reset", invincibleDelay);
+            }
         }
     }
     void OnCollisionEnter2D(Collision2D collision)
@@ -53,13 +62,15 @@ public class PlayerV2 : MonoBehaviour
         if (collision.collider.CompareTag("Ground"))
         {
             print("Ground");
-            if (collision.GetContact(0).point.y < player.transform.position.y)
-            {
-                can_jump = true;
-            }
+            can_jump = true;
         }
     }
+    void Reset()
+    {
+        gotHit = false;
+    }
 }
+
 //to avoid gettign stuck on wall make a new physics material and make no friction
     //instatiate is what you use to spawn things
     /*if(other.collider.CompareTag("tag"))
