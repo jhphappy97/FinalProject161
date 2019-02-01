d3d532d62b1f07cebde16ef27a468c06feba73e0
Marco Cendejas <HomicideMojado@dhcp-v126-022.mobile.uci.edu>
2019-02-01 15:49:39 -0800

    new

---
+++ Space Invaders/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+public width,height;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+void spawnObjects
+{
+    for(int x=0;x<width;x++)
+    {
+        for(int y=0;y<height;++)
+        {
+            int randomIndex =
+        }   
+    }
+}
+}
