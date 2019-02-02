8f0b0b6aef622df3c8723cfa05531ad375c3c9c2
jhphappy97 <hnphan1@uci.edu>
2019-02-01 19:42:23 -0800

    new wall and goes down `

--- Space Invaders/Assets/Scripts/EnemySpawner.cs
+++ Space Invaders/Assets/Scripts/EnemySpawner.cs
@@ -5,34 +5,76 @@ using UnityEngine.SceneManagement;
 
 public class EnemySpawner : MonoBehaviour
 {
+    private const float V = 0.3f;
     public int width,height;
     public GameObject enemy1;
-   
+    public float speed = 2;
+    private enenmy enenmy_access;
     private Rigidbody2D m_rigidbody;
-    private bool enemy_wallcollide = false;
-
+    public GameObject Enemy_script;
     // Start is called before the first frame update
-    void Start()
+     void Awake()
     {
         spawnObjects();
+        enenmy_access = Enemy_script.GetComponent<enenmy>();
+
+    }
+    void Start()
+    {
+        GameObject[] obj = GameObject.FindGameObjectsWithTag("Enemy");
     }
 
     // Update is called once per frame
 
     void spawnObjects()
-{
-    for(int x=0;x<width;x++)
     {
-        for(int y=0;y<height;y++)
+        for (int x = 0; x < width; x++)
         {
-                Vector3 Spawnposition = new Vector3(x, y, 0);
-                GameObject newEnemy = Instantiate(enemy1, this.transform);
-                newEnemy.transform.localPosition = Spawnposition+ new Vector3(1,1,0);
-            }
-        }
-}
 
-   
-   
+
+            for (int y = 0; y < height; y++)
+            {
+                Vector3 Spawnposition = new Vector3(x + V * x, y, 0);
+                GameObject newEnemy = Instantiate(enemy1, this.transform);
+                newEnemy.transform.localPosition = Spawnposition;
+            }
+
+        }
+    }
     
+    void go_right()
+    {
+        GameObject[] obj = GameObject.FindGameObjectsWithTag("Enemy");
+        for(int i=0;i<obj.Length;i++)
+        {
+            obj[i].transform.Translate(Vector3.right * speed * Time.deltaTime);
+
+        }
+    }
+    void go_left()
+    {
+        GameObject[] obj = GameObject.FindGameObjectsWithTag("Enemy");
+        for (int i = 0; i < obj.Length; i++)
+        {
+            obj[i].transform.Translate(Vector3.left * speed * Time.deltaTime);
+
+        }
+    }
+   
+    void Update()
+    {
+        if (enenmy_access.enemy_wallcollide)
+        {
+            go_right();
+        }
+        else
+        {
+            go_left();
+        }
+    }
+
+
+
+
+
 }
--- Space Invaders/Assets/Scripts/enenmy.cs
+++ Space Invaders/Assets/Scripts/enenmy.cs
@@ -5,40 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class enenmy : MonoBehaviour
 {
-    private bool enemy_wallcollide = false;
+    public bool enemy_wallcollide = false;
     public float speed = 2;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-    void go_right()
-    {
-        this.transform.Translate(Vector3.right * speed * Time.deltaTime);
-    }
-    void go_left()
-    {
-        this.transform.Translate(Vector3.left * speed * Time.deltaTime);
-    }
-    void Update()
-    {
-        if (enemy_wallcollide)
-        {
-            go_right();
-        }
-        else
-        {
-            go_left();
-        }
-    }
+    
     // Update is called once per frame
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("entering wall");
         if (other.CompareTag("Wall"))
         {
-            Debug.Log("2");
             enemy_wallcollide = !enemy_wallcollide;
+            this.transform.Translate(0.0f, -1.0f, 0.0f);
+
         }
     }
     private void OnCollisionEnter2D(Collision2D other)
