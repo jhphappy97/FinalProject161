1f3341693a8b5ec2084183444b1a78637f130595
Marco Cendejas <HomicideMojado@dhcp-v126-022.mobile.uci.edu>
2019-02-01 17:56:47 -0800

    player

--- Space Invaders/Assets/Scripts/Player.cs
+++ Space Invaders/Assets/Scripts/Player.cs
@@ -6,15 +6,17 @@ public class Player : MonoBehaviour
 {
 
     private Rigidbody2D player;
+    //[SerializeField]private Rigidbody2D bullet;
     private bool gotHit = false;
     //private Text score_text;
     //private Text health_text;
     [SerializeField] private float speed = 2.0f;
+    [SerializeField] private float bulletSpeed = 2.0f;
     [SerializeField] private int health = 3;
     [SerializeField] private int score = 0;
     [SerializeField] private int invincibleDelay = 3;
-    public GameObject scoreGameObject;
-    public GameObject healthGameObject;
+    //public GameObject scoreGameObject;
+    //public GameObject healthGameObject;
 
     // Start is called before the first frame update
     void Start()
