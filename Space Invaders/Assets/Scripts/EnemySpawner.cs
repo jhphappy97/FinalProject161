0843fb13b6316a8d07dbb37d7d644b4698729cf8
jhphappy97 <hnphan1@uci.edu>
2019-02-01 16:16:40 -0800

    added enemy

--- Space Invaders/Assets/Scripts/EnemySpawner.cs
+++ Space Invaders/Assets/Scripts/EnemySpawner.cs
@@ -1,29 +1,65 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EnemySpawner : MonoBehaviour
 {
-public width,height;
+    public int width,height;
+    public GameObject enemy1;
+    public float speed = 2;
+    private Rigidbody2D m_rigidbody;
+    private bool enemy_wallcollide = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnObjects();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
-void spawnObjects
+    
+    void spawnObjects()
 {
     for(int x=0;x<width;x++)
     {
-        for(int y=0;y<height;++)
+        for(int y=0;y<height;y++)
         {
-            int randomIndex =
+                Vector3 Spawnposition = new Vector3(x+1, y+1, 0);
+                GameObject newEnemy = Instantiate(enemy1, this.transform);
+                newEnemy.transform.localPosition = Spawnposition;
         }   
     }
 }
+
+    void go_right()
+    {
+        this.transform.Translate(Vector3.right * speed * Time.deltaTime);
+    }
+    void go_left()
+    {
+        this.transform.Translate(Vector3.left * speed * Time.deltaTime);
+    }
+    void Update()
+    {
+        if(enemy_wallcollide)
+        {
+            go_right();
+        }
+        else
+        {
+            go_left();
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.collider.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        if(other.collider.CompareTag("Wall"))
+        {
+            enemy_wallcollide = !enemy_wallcollide;
+        }
+    }
 }
