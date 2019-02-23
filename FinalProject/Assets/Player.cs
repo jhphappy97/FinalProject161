38eaedbd127a0def630823645f1f234ee4889aba
Li Zhang <zhangl16@uci.edu>
2019-02-22 20:32:57 -0800

    player

--- FinalProject/Assets/Player.cs
+++ FinalProject/Assets/Player.cs
@@ -4,15 +4,71 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private SpriteRenderer m_spriteRenderer;
+    private Rigidbody2D m_rigidbody;
+    private Collider2D m_collider;
+    private bool isGrounded = false;
+    [SerializeField] protected float speed = 5;
+    [SerializeField] protected float jumpforce = 7.5f;
     // Start is called before the first frame update
+    private void Awake()
+    {
+        m_spriteRenderer = this.GetComponent<SpriteRenderer>();
+    }
     void Start()
     {
-        
+        m_rigidbody = this.GetComponent<Rigidbody2D>();
+        m_collider = this.GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+        if (Input.GetKeyDown(KeyCode.Space)&& isGrounded)
+        {
+            Jump();
+        }
+
+    }
+    void Jump()
+    {
+        isGrounded =  false;
+        m_rigidbody.AddForce(Vector2.up * jumpforce, ForceMode2D.Impulse);
+    }
+
+    void Move()
+    {
+        float movemnetModifier = Input.GetAxis("Horizontal");
+        if(movemnetModifier > 0)
+        {
+            m_spriteRenderer.flipX = false;
+        }
+        else if(movemnetModifier < 0)
+        {
+            m_spriteRenderer.flipX = true;
+        }
+        Vector2 currentVelocity = m_rigidbody.velocity;
+        m_rigidbody.velocity = new Vector2(movemnetModifier * speed, currentVelocity.y);
+
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if(collision.collider.CompareTag("ground"))
+        {
+            Vector2 feetPosition = new Vector2(this.transform.position.x, m_collider.bounds.min.y);
+            RaycastHit2D hitInfo = Physics2D.Raycast(feetPosition, Vector2.down, 0.1f);
+            if(hitInfo && hitInfo.collider.CompareTag("ground"))
+            {
+                isGrounded = true;
+            }
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.collider.CompareTag("ground"))
+        {
+            isGrounded = false;
+        }
     }
 }
