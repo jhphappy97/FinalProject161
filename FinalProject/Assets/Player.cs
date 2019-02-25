c268c69308b3a24274a033fdb41e2c2f46fec26c
Li Zhang <zhangl16@uci.edu>
2019-02-24 17:27:35 -0800

    bullet
    bullet movement going downwards not fixed yet.

--- FinalProject/Assets/Player.cs
+++ FinalProject/Assets/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Transform fp;
+    private bool facingright = true;
     private SpriteRenderer m_spriteRenderer;
     private Rigidbody2D m_rigidbody;
     private Collider2D m_collider;
@@ -11,6 +13,10 @@ public class Player : MonoBehaviour
     private Animator anim;
     [SerializeField] protected float speed = 5;
     [SerializeField] protected float jumpforce = 7.5f;
+    private bool hitsnowile = false;
+    private bool shootstatus = false;
+    public GameObject bullet;
+    [SerializeField] protected float bulletspeed = 0.1f;
     // Start is called before the first frame update
     private void Awake()
     {
@@ -31,8 +37,23 @@ public class Player : MonoBehaviour
         {
             Jump();
         }
+        if (Input.GetKeyDown(KeyCode.F) && hitsnowile == true)
+        {
+            getbullet();
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            shoot();
+        }
+
 
     }
+    void shoot()
+    {
+        Debug.Log("shoot");
+        GameObject b = Instantiate(bullet, this.transform.position, Quaternion.identity);
+        b.GetComponent<Rigidbody2D>().AddForce()
+        }
     void Jump()
     {
         isGrounded =  false;
@@ -48,29 +69,36 @@ public class Player : MonoBehaviour
         }
         if(movemnetModifier > 0)
         {
-            m_spriteRenderer.flipX = false;
+           // m_spriteRenderer.flipX = false;
             anim.SetBool("walk",true);
         }
         else if(movemnetModifier < 0)
         {
-            m_spriteRenderer.flipX = true;
+            //\\m_spriteRenderer.flipX = true;
             anim.SetBool("walk",true);
         }
         Vector2 currentVelocity = m_rigidbody.velocity;
         m_rigidbody.velocity = new Vector2(movemnetModifier * speed, currentVelocity.y);
-
+        if (movemnetModifier > 0 && !facingright || movemnetModifier < 0 && facingright)
+        {
+            Flip();
+        }
     }
     private void OnCollisionStay2D(Collision2D collision)
     {
         if(collision.collider.CompareTag("ground"))
         {
-            Vector2 feetPosition = new Vector2(this.transform.position.x, m_collider.bounds.min.y);
+            Vector2 feetPosition = new Vector2(this.transform.position.x, m_collider.bounds.min.y-0.1f);
             RaycastHit2D hitInfo = Physics2D.Raycast(feetPosition, Vector2.down, 0.1f);
             if(hitInfo && hitInfo.collider.CompareTag("ground"))
             {
                 isGrounded = true;
                 anim.SetBool("jump",false);
             }
+        }
+        if(collision.collider.CompareTag("Snowpile"))
+        {
+            hitsnowile = true;
         }
     }
     private void OnCollisionExit2D(Collision2D collision)
@@ -80,4 +108,14 @@ public class Player : MonoBehaviour
             isGrounded = false;
         }
     }
+    private void getbullet()
+    {
+        shootstatus = true;
+    }
+    private void Flip()
+    {
+        facingright = !facingright;
+        transform.Rotate(0f, 180f, 0f);
+    }
+
 }
---
+++ FinalProject/Assets/bul.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bul : MonoBehaviour
+{
+    public float speed = 0.1f;
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
