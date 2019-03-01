1c375a7854c9e62a77cb9cbbfa1bff8f0238a755
李艺安 <yianl2@uci.edu>
2019-02-28 19:23:27 -0800

    need to add enemy knockback

--- FinalProject/Assets/Scripts/Monster2.cs
+++ FinalProject/Assets/Scripts/Monster2.cs
@@ -13,6 +13,8 @@ public class Monster2 : MonoBehaviour
     private float timerForFire, timerForHit;
     private bool right = false;
     private bool canFire = true;
+    
+    public float back=5;
     // Start is called before the first frame update
     void Start()
     {
@@ -99,6 +101,10 @@ public class Monster2 : MonoBehaviour
         if (collision.collider.CompareTag("Player"))
         {
             gotHit = true;
+        }
+        if (collision.collider.CompareTag("bullet"))
+        {
+            enemy.velocity = new Vector2(-enemy.velocity.x, enemy.velocity.y);
         }
     }
 }
